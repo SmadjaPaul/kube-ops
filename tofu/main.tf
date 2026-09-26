167dc7789b79d0bb7389c5ef6e92676aa4392d40
1kuz0 <42036416+SmadjaPaul@users.noreply.github.com>
2026-09-26 17:46:08 +0200

    chore(v1): bootstrap Kubernetes API on control-plane IP

--- tofu/main.tf
+++ tofu/main.tf
@@ -59,7 +59,7 @@ module "talos" {
 
   cluster = {
     name               = var.cluster_name
-    endpoint           = coalesce(var.external_api_endpoint, "api.${var.cluster_domain}")
+    endpoint           = [for name, n in var.nodes_config : n.ip if n.machine_type == "controlplane"][0]
     gateway            = var.network.gateway
     vip                = var.network.vip
     talos_version      = var.versions.talos
